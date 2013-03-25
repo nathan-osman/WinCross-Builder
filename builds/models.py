3b4ebf61ddd1d38a354c212bff25235fb3db85f3
Nathan Osman <admin@quickmediasolutions.com>
2013-03-24 17:31:45 -0700

    Added empty 'packages' and 'builds' apps.

---
+++ builds/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Build(models.Model):
+    '''Represents a build of a particular package.'''
---
+++ packages/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Package(models.Model):
+    '''Represents a package available for building.'''
