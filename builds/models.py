d6d4a920403231acdc7e73aea82590ca26f6e9c5
Nathan Osman <admin@quickmediasolutions.com>
2013-03-24 22:35:42 -0700

    Began filling in models.

--- builds/models.py
+++ builds/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+from packages.models import Version
+
 class Build(models.Model):
-    '''Represents a build of a particular package.'''
+    '''Represents a build of a particular package version.'''
+    
+    models.ForeignKey(Version, help_text='The package version that this build belongs to')
--- packages/models.py
+++ packages/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 class Package(models.Model):
     '''Represents a package available for building.'''
+    
+    name        = models.CharField(max_length=40, help_text='The name of the package')
+    website     = models.URLField(help_text='A website where more information about the package can be found.')
+    description = models.TextField(help_text='Information about the package')
+    
+    def __unicode__(self):
+        '''Returns a string representation of the package.'''
+        return self.name
+
+class Version(models.Model):
+    '''Represents a particular version of a package.'''
+    
+    package      = models.ForeignKey(Package, related_name='', help_text='The package represented by the version')
+    version      = models.CharField(max_length=20, help_text='The version string of the package')
+    release_date = models.DateField(help_text='The date that the version was released')
+    
+    def __unicode__(self):
+        '''Returns a string representation of the version.'''
+        return self.version
